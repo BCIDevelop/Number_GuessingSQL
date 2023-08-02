4151b393cbc3e7b257f9f28802929be7a14b03e0
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-02 03:21:40 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+echo Enter your username:
+read USERNAME
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+
