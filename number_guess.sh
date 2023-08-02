9bd36a5932ca1df85b4910d7186279a672718231
BCIDevelop <luis.lopez@utec.edu.pe>
2023-08-02 03:22:47 +0000

    feat: User info added

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,14 @@ echo Enter your username:
 read USERNAME
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
+if [[ -z $USER_ID ]]
+then
+  ADD_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  GAME_INFO=$($PSQL "SELECT COUNT(*),MIN(guesses_to_win) FROM users INNER JOIN games USING(user_id) WHERE user_id=$USER_ID GROUP BY user_id")
+
+  IFS='|' read PLAYS BEST <<< $GAME_INFO
+  echo Welcome back, $USERNAME! You have played $PLAYS games, and your best game took $BEST guesses.
+fi
